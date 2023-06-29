2722125f20d62eef90981387d14512fba2898659
Joey van Hoek <joey@dimenco.eu>
2023-06-29 11:33:36 +0200

    Added folder structure and main file

---
+++ game_bridge_injection_cli/src/main.cpp
@@ -0,0 +1,45 @@
+#include <Windows.h>
+
+/*
+ * Todo:
+ * Parse environment vars and arguments
+ * Load config file for supported games
+ * Display supported games
+ * Listen for processes
+ * Runtime injection into process
+ *
+ * Menu?
+ */
+
+// command_line_arguments.cpp
+// compile with: /EHsc
+#include <iostream>
+
+using namespace std;
+int main(int argc,      // Number of strings in array argv
+    char* argv[],   // Array of command-line argument strings
+    char* envp[])  // Array of environment variable strings
+{
+    int count;
+
+    // Display each command-line argument.
+    cout << "\nCommand-line arguments:\n";
+    for (count = 0; count < argc; count++)
+        cout << "  argv[" << count << "]   "
+        << argv[count] << "\n";
+
+    bool numberLines = false;    // Default is no line numbers.
+
+    // If /n is passed to the .exe, display numbered listing
+    // of environment variables.
+    if ((argc == 2) && _stricmp(argv[1], "/n") == 0)
+        numberLines = true;
+
+    // Walk through list of strings until a NULL is encountered.
+    for (int i = 0; envp[i] != NULL; ++i)
+    {
+        if (numberLines)
+            cout << i << ": "; // Prefix with numbers if /n specified
+        cout << envp[i] << "\n";
+    }
+}
