16a9a8c8f86a17b2b4cb6552ef95277790af753f
Joey <jvanhoek@hotmail.com>
2023-07-06 14:29:52 +0800

    Minor cleanup

--- game_bridge_api/src/wmicommunication.h
+++ game_bridge_api/src/wmicommunication.h
@@ -2,6 +2,8 @@
 #include <Windows.h>
 #include <Wbemidl.h>
 
+// Todo figure out this code more
+
 class EventSink : public IWbemObjectSink
 {
     LONG m_lRef;
--- game_bridge_injection_cli/src/app.cpp
+++ game_bridge_injection_cli/src/app.cpp
@@ -5,12 +5,15 @@
 namespace game_bridge {
     GameBridgeInjectionCLI::GameBridgeInjectionCLI()
     {
+        // Load configuration
         InitializeConfiguration();
         supported_games = LoadConfiguration();
 
+        // Initialize Game Bride Api
         game_bridge::init_api();
         game_bridge::subscribe_to_pocess_events();
 
+        // Keep theapp running
         while (true) {
             Sleep(1000);
         }
