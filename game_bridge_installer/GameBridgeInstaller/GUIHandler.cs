8e774a3f44bb243f3be82142ee5541b1beddfe95
BramTeurlings <bram@dimenco.eu>
2023-12-08 11:22:19 +0100

    Program always starts up as administrator now

--- game_bridge_installer/GameBridgeInstaller/GUIHandler.cs
+++ game_bridge_installer/GameBridgeInstaller/GUIHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
